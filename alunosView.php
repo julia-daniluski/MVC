a3a5875e732f7547952765c2bb64a3cfbd120921
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-14 15:10:34 -0300

    Update alunosView.php

--- alunosView.php
+++ alunosView.php
@@ -1,20 +1,19 @@
 <?php
-// Declara uma função chamada exibirAlunos que recebe o parâmetro $alunos.
-// Esse parâmetro é esperado como um array com informações dos alunos.
+//Declara uma função chamada exibirAlunos que recebe o parâmetro $alunos
+//Esse parâmetro é esperado com um array com um array com informações dos alunos.
 
-function exibirAlunos($alunos) {
-    // Imprime na tela um título h2 e abre uma lista não ordenada <ul>.
-    echo "<h2>Lista de alunos:</h2><ul>";
+function exibirAlunos($alunos){
+    // Imprime na tela um titulo h2 e abre uma lista não ordenada ul.
+    echo "<h2> Lista de Alunos: </h2> <ul>";
 
-    // Inicia um loop foreach que percorre cada item do array $alunos.
-    // Cada item é armazenado temporariamente na variável $aluno.
-    foreach ($alunos as $aluno) {
-        // Para cada aluno, imprime um item na lista <li>.
-        // Exibe o nome do aluno e sua idade, formatados como "nome - idade anos".
-        echo "<li>{$aluno['nome']} - {$aluno['idade']} anos</li>";
+    //Inicia um loop foreach, que percorre cada item do array $alunos.
+    //Cada item é armazenado temporariamente na variavel $aluno
+    foreach ($alunos as $aluno ){
+        //para cada aluno, imprime um item da lista(li).
+        // Exibe o nome do aluno e sua idade, formatados como "Nome - Idade anos".
+        echo "<li> {$aluno['nome']} - {$aluno['idade']} anos</li>";
     }
-
-    // Fecha a lista <ul>.
     echo "</ul>";
 }
+
 ?>
