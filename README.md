d2ddca5c0fe8dd51c1e0758290089f3fced8d921
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-14 14:04:24 -0300

    uii

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MVC
  
+## O que é MVC?
+
+* M: Model
+* V: View
+* C: Controler
+
+É um padrão de arquitetura, usado para organizar o código de forma clara, separando a lógica em 3 partes.
+
+## Esquema MVC
+
+usuários(navegador) -> Requisição -> controler -> model -> controler -> view -> usuarios.
+
---
+++ aluno.php
@@ -0,0 +1,12 @@
+<?php
+// Defina uma classe chamada aluno
+class Aluno{
+    // criar uma propriedade privada (encapsulamento) chamada $alunos;
+    // Essa propriedade é um array que armazena dados e alunos;
+    // Cada aluno é representado por um array associativo com nome e idade
+private $alunos = [
+    ['nome' => 'Ferreirinha', 'idade' => 16],
+    ['nome' => 'Lucas', 'idade' => 17],
+    ['nome' => 'Oscar', 'idade' => 18]
+];
+}
