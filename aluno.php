e6f757fe671954c64cf4588612e9ad8b37d8c0dd
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-14 15:14:00 -0300

    Update aluno.php

--- aluno.php
+++ aluno.php
@@ -5,9 +5,9 @@ class Aluno{
     // Essa propriedade é um array que armazena dados e alunos;
     // Cada aluno é representado por um array associativo com nome e idade
 private $alunos = [
-    ['nome' => 'Ferreirinha', 'idade' => 16],
-    ['nome' => 'Lucas', 'idade' => 17],
-    ['nome' => 'Oscar', 'idade' => 18]
+    ['nome' => 'Ferreirinha', 'idade' => 27],
+    ['nome' => 'Lucas', 'idade' => 32],
+    ['nome' => 'Oscar', 'idade' => 33]
 ];
 
 // Define um método púnlico chamado listarAlunos.
