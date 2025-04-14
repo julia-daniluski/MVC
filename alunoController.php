2e7ad62db9510f683a7ea714d7483dd5d9a436ec
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-14 15:09:28 -0300

    acho que ta certo

--- alunoController.php
+++ alunoController.php
@@ -1,34 +1,39 @@
 <?php
-// incluir o arquivo 'aluno.php' que contem a classe Aluno.
-// Incluir o alunosView.php que contém a função exibirAlunos().
+    //Incluir o arquivo 'aluno.php' que contem a classe Aluno.
+    //Incluir o alunosView.php, que contem a função exibirAlunos().
+    require_once 'aluno.php';
+    require_once 'alunosView.php';
 
-require_once 'aluno.php';
-require_once 'alunosView.php';
+    // Declaração da clase alunoController.
+    // Essa classe é responsável pela comunicação entre o aluno.php (Model) e o alunosView(View)
+    class alunoController{
+        //Criar uma propriedadeprivada chamada $model
+        //|Ela será usada para armazenaruma instancia da classe aluno.
+        private $model;
 
-// Declaração da classe alunoController.
-// Essa classe é responsável pela comunicação entre aluno.php(model) e p alunosView.php(view).
+        //Criar um método constructor da classe, ele é chamado automaticamente quando um objeto é criado.
+        public function __construct()
+        {
+            //criar uma nova instancia da classe aluno, e armazenar na propriedade $model
+            $this->model= new Aluno();
+        }
 
-class alunoController{
-    // criar uma propriedade privada chamada $model.
-    // ela será usada para armazenar uma instancai da classe aluno.
+        //Método público chamado listar.
+        // Serve para buscar a lista de alunos e passar para o view.
+        public function listar(){
+            //chama o método listarAluno() do modelo e armazena na variável $alunos.
+            $alunos = $this->model->listarAlunos();
+            // chama a função exibir alunos e envia a lista para mostrar no navegador.
 
-private $model;
-// criar um metodo constructor da classe, ele é chamado automaticamente quando um objeto for criado.
-
-public function __construct()
-{
-    // criar uma nova instancia da classe aluno e armazenar na propriedade $model
-    $this->model= new Aluno();
-
-    //metodo publico chamado listar que serve para buscar a lista de alunos e passar para o view
-
-    public function listar(){
-        // chama o metodo listarAlunos() do modelo e armazena na variavel $alunos.
-        $alunos = $this->model->listarAlunos();
-        // chama a função exibir alunos e envia para mostrar no navegador
+            exibirAlunos($alunos);
+        }
     }
-}
-}
+
+    //execução do codigo
+    //aqui fora da classe é criado um objeto do tipo AlunoController();
+    $controler = new alunoController();
+    // Após criar o Objeto, é chamado o método  listar();
+    $controler->listar();
 
 
 ?>
